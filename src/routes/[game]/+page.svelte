ca691362a4eb7757a4e46f3a4df4a10c279704d1
LeAmering <91052718+LeAmering@users.noreply.github.com>
2025-01-17 06:15:21 +0100

    gameLayout

--- src/routes/[game]/+page.svelte
+++ src/routes/[game]/+page.svelte
@@ -1,3 +1,11 @@
-<main class="bg-bgBlue flex h-screen flex-col items-center justify-center text-gray-100">
+<main class="flex h-screen flex-col items-center justify-center bg-bgBlue text-gray-100">
 	<h1 class="text-white">Welcome to the Player vs Player page!</h1>
+
+	<div class="h-4/5 w-2/6 bg-white">
+		<div></div>
+
+		<div></div>
+
+		<div></div>
+	</div>
 </main>
