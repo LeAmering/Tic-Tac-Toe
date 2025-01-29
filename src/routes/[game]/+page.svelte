bb085c2df7f8439bfc9b8d9d5dfec94a7a3ea722
LeAmering <91052718+LeAmering@users.noreply.github.com>
2025-01-29 19:18:31 +0100

    scores+modal

--- src/routes/[game]/+page.svelte
+++ src/routes/[game]/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { chosenPlayer, decideVS } from '$lib/stores'; // Import both stores
+	import { goto } from '$app/navigation'; // Import goto function
 	import { onMount } from 'svelte';
 	let currentPlayer = 'X';
 	let board = Array(9).fill('');
@@ -29,6 +30,10 @@
 		}
 	}
 
+	function returnHome() {
+		goto('/'); // Navigates to the homepage
+	}
+
 	function onPageLoad() {
 		if ($decideVS === 'cpu' && $chosenPlayer === 'O') {
 			cpuMove();
@@ -45,7 +50,7 @@
 				board = [...board];
 
 				if ($decideVS === 'cpu' && $chosenPlayer !== currentPlayer && !winner) {
-					setTimeout(cpuMove, 100);
+					setTimeout(cpuMove, 400);
 				}
 			}
 		}
@@ -95,31 +100,68 @@
 
 		if (board.every((cell) => cell !== '')) {
 			isDraw = true;
+			drawMod.showModal();
 			scoreTies++;
 		}
 	}
 
 	function restartGame() {
 		board = Array(9).fill('');
-		currentPlayer = $chosenPlayer;
+		currentPlayer = 'X';
 		winner = null;
 		isDraw = false;
 
-		if ($decideVS === 'cpu' && $chosenPlayer === 'o') {
+		if ($decideVS === 'cpu' && $chosenPlayer === 'O') {
 			cpuMove();
 		}
 	}
 </script>
 
 <main class="flex h-screen flex-col items-center justify-center bg-bgBlue text-gray-100">
-	<p class="mt-4 text-xl">Current Turn: Player {currentPlayer}</p>
-
-	<button
-		class="mt-6 rounded bg-bgBlue px-4 py-2 text-white shadow-md hover:bg-green-600"
-		on:click={restartGame}
-	>
-		Restart Game
-	</button>
+	<div class="relative mt-1 grid w-80 grid-cols-3 gap-2">
+		<img src="assets/images/SVG/logo.svg" alt="x" class="h-12 w-12" />
+		<div
+			class="flex h-8 w-20 items-center justify-center rounded bg-lgBlue text-[10px] font-bold text-grayTie shadow-custom-shadowBlue"
+		>
+			{#if currentPlayer === 'X'}
+				<svg
+					id="icon-x-default"
+					viewBox="0 0 64 64"
+					xmlns="http://www.w3.org/2000/svg"
+					width="10"
+					height="10"
+					class="mr-2"
+				>
+					<path
+						d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z"
+						fill="#A8BEC9"
+						fill-rule="evenodd"
+					/>
+				</svg>
+			{:else}
+				<svg
+					id="icon-o-default"
+					viewBox="0 0 64 64"
+					xmlns="http://www.w3.org/2000/svg"
+					width="10"
+					height="10"
+					class="mr-2"
+				>
+					<path
+						d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"
+						fill="#A8BEC9"
+					/>
+				</svg>
+			{/if}
+			<span>TURN</span>
+		</div>
+		<button
+			class="absolute right-0 top-0 mr-1 mt-1 h-8 w-8 items-center justify-center rounded bg-grayTie text-white shadow-md"
+			on:click={restartGame}
+		>
+			<img src="assets/images/SVG/icon-restart.svg" alt="x" class="mx-auto h-4 w-4" />
+		</button>
+	</div>
 
 	<div class="grid h-80 w-80 grid-cols-3 gap-2">
 		{#each board as cell, index}
@@ -155,7 +197,7 @@
 		</div>
 
 		<div
-			class="bg-grayTie flex h-12 w-20 flex-col items-center justify-center rounded-lg text-[8px] text-black shadow-md"
+			class="flex h-12 w-20 flex-col items-center justify-center rounded-lg bg-grayTie text-[8px] text-black shadow-md"
 		>
 			<p>TIES</p>
 			<p class="text-sm font-bold">{scoreTies}</p>
@@ -179,20 +221,50 @@
 	</div>
 
 	<dialog id="winMod" class="modal fixed inset-0 flex items-center justify-center">
-		<div class="modal-box box-border flex w-full max-w-full items-center justify-center p-6">
-			<p>YOU WON!</p>
-			<br />
-			{#if winner === 'X'}
-				<img src="assets/images/SVG/icon-x.svg" alt="X" class="m-3 h-12 w-12" />
-			{:else if winner === 'O'}
-				<img src="assets/images/SVG/icon-o.svg" alt="O" class="m-3 h-12 w-12" />
+		<div
+			class="modal-box box-border flex w-full max-w-full flex-col items-center justify-center space-y-4 p-6"
+		>
+			{#if $decideVS === 'player'}
+				{#if winner === $chosenPlayer}
+					<p class="text-lg font-bold">PLAYER 1 WON</p>
+				{:else}
+					<p class="text-lg font-bold">PLAYER 2 WON</p>
+				{/if}
+			{:else if winner === $chosenPlayer}
+				<p class="text-lg font-bold">YOU WON</p>
+			{:else}
+				<p class="text-lg font-bold">CPU WON</p>
 			{/if}
-			<p>takes the round</p>
+
 			<br />
-			<div class="modal-action">
-				<form method="dialog">
-					<button class="btn">Close</button>
-				</form>
+
+			<div class="flex items-center gap-2">
+				{#if winner === 'X'}
+					<img src="assets/images/SVG/icon-x.svg" alt="X" class="m-3 h-12 w-12" />
+					<p class="font-bold text-grBlue">TAKES THE ROUND</p>
+				{:else if winner === 'O'}
+					<img src="assets/images/SVG/icon-o.svg" alt="O" class="m-3 h-12 w-12" />
+					<p class="font-bold text-yellow-500">TAKES THE ROUND</p>
+				{/if}
+			</div>
+
+			<div class="flex gap-4">
+				<button
+					class="h-8 w-20 items-center justify-center rounded bg-grayTie text-black shadow-custom-shadowBlue"
+					on:click={returnHome}
+				>
+					QUIT
+				</button>
+				<div class="modal-action">
+					<form method="dialog">
+						<button
+							class="h-8 w-20 items-center justify-center rounded bg-yellow-500 text-black shadow-custom-shadowBlue"
+							on:click={restartGame}
+						>
+							NEXT ROUND
+						</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	</dialog>
